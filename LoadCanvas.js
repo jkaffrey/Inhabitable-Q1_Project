e04d0fcac6529461a109a9ef73561a316edf0556
Jeremy <jkaffrey@gmail.com>
2016-05-26 14:14:17 -0600

    More styling and a login page

--- LoadCanvas.js
+++ LoadCanvas.js
@@ -1,8 +1,29 @@
 "use strict";
+var BackgroundScroll = function(params) {
+  params = $.extend({
+    scrollSpeed: 70,
+    imageWidth: $('#background').width(),
+    imageHeight: $('#background').height()
+  }, params);
 
-$(function() {
+  var step = 1,
+  current = 0,
+  restartPosition = - (params.imageWidth - params.imageHeight);
 
-  var cxt = document.getElementById('canvas').getContext('2d');
-  cxt.canvas.width = window.innerWidth;
-  cxt.canvas.height = window.innerHeight;
-});
+  var scroll = function() {
+    current -= step;
+    if (current == restartPosition){
+      current = 0;
+    }
+    $('#background').css('backgroundPosition', current + 'px 0');
+
+  };
+
+  this.init = function() {
+    setInterval(scroll, params.scrollSpeed);
+
+  };
+};
+
+var scroll = new BackgroundScroll();
+scroll.init();
