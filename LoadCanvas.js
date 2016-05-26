a93a628bb614b11f570ee41e35ebb35f1c851b41
Jeremy <jkaffrey@gmail.com>
2016-05-26 13:48:15 -0600

    Styling almost done

---
+++ LoadCanvas.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(function() {
+
+  var cxt = document.getElementById('canvas').getContext('2d');
+  cxt.canvas.width = window.innerWidth;
+  cxt.canvas.height = window.innerHeight;
+});
